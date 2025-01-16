150658c6c58aecdedde6d5e2122c44fa783d6dd2
Harsh Katiyar <96325545+Harshkatiyar077@users.noreply.github.com>
2025-01-16 06:37:17 +0530

    Update truthy.js
    notes added lecture 26

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -10,32 +10,37 @@ if (userEmail) {
 
 // false, 0, -0, BigInt 0n, "", null, undefined, NaN
 
-//truthy values
+//truthy values These values are considered as true when used in conditions
 // "0", 'false', " ", [], {}, function(){}
 
 // if (userEmail.length === 0) {
-//     console.log("Array is empty");
-// }
+//     console.log("Array is empty");  to check if array is empty
+// }  
 
 const emptyObj = {}
 
-if (Object.keys(emptyObj).length === 0) {
+if (Object.keys(emptyObj).length === 0) {   
     console.log("Object is empty");
-}
+}  // It's to check if object is empty
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------------
+
 
 // Nullish Coalescing Operator (??): null undefined
-
+   // it is to assign a number instead of null it will assign firstly received number if no number is received then it will assign null or udefined.
 let val1;
 // val1 = 5 ?? 10
 // val1 = null ?? 10
 // val1 = undefined ?? 15
 val1 = null ?? 10 ?? 20
 
+------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 console.log(val1);
 
-// Terniary Operator
+// Terniary Operator IS different from nullish, Its like if else.
 
 // condition ? true : false
 
