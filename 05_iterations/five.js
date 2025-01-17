b2ef935be38a81d5d70bd5db05932f88b57d262f
Harsh Katiyar <96325545+Harshkatiyar077@users.noreply.github.com>
2025-01-17 00:09:27 +0000

    added notes for each loop

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -4,18 +4,18 @@ const coding = ["js", "ruby", "java", "python", "cpp"]
 //     console.log(val);
 // } )
 
-// coding.forEach( (item) => {
+// coding.forEach( (item) => {        //its the example with arrow function{ H.W - revise arrow function}
 //     console.log(item);
 // } )
 
 // function printMe(item){
-//     console.log(item);
+//     console.log(item);     //here we are declaring function 
 // }
 
-// coding.forEach(printMe)
+//   coding.forEach(printMe)  //here we are passing call back function, not have to write () just pass function name (reference)
 
 // coding.forEach( (item, index, arr)=> {
-//     console.log(item, index, arr);
+//     console.log(item, index, arr);  here we can see this dont get only value but it also have access to index and full array
 // } )
 
 const myCoding = [
@@ -37,3 +37,8 @@ myCoding.forEach( (item) => {
     
     console.log(item.languageName);
 } )
+
+// it is how we access objects inside array using for each loop. 
+
+// it is very important to access DB using for each.
+
